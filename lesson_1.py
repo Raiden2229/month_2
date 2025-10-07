ee141e324c52fb2d8f73448a62048effce827e76
Raiden2229 <pirmatovdair@gmail.com>
2025-10-07 15:07:59 +0300

    first commit

---
+++ lesson_1.py
@@ -0,0 +1,20 @@
+class Car:
+    def __belf__(self, model, color):
+        self.model = model
+        self.color = color
+
+car_honda = Car(model = 'Honda, color = White' )
+car_subaru = Car(color = 'Red, model = subaru')
+
+print(car_honda)
+print(f'Car model: {car_honda.model}, Car color: {car_honda.color}')
+print(car_subaru)
+print(f'Car model: {car_subaru.model}, Car color: {car_subaru.color}')
+
+
+
+
+
+# def myfunc():
+#     pass
+# myfunc()
